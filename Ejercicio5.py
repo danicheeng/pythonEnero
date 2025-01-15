aa318989e7e760632b5bc29c8fddabc039112574
ciberkaos <danielchengliu@gmail.com>
2025-01-15 11:48:53 +0000

    push

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,6 +1,6 @@
 numero = int(input("Ingresa un numero entre 1 y 100"))
 
 if numero > 0 and numero <= 100:
-    print("Es un numero correcto")
+    print("El número está dentro del rango")
 else:
     print("El número está fuera del rango")
---
+++ Ejercicio6.py
@@ -0,0 +1,6 @@
+letra = input ("Dame una letra")
+
+if letra == 'a' and letra = 'e' and letra = 'i' and letra = 'o' and letra = 'u':
+    print("Es una vocal")
+else:
+    print("No es vocal")
