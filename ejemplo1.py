4be6bb4cdf5e61b21a569b9c897005e6993a4f63
ciberkaos <danielchengliu@gmail.com>
2025-01-15 12:47:55 +0000

    push

---
+++ ejemplo1.py
@@ -0,0 +1,29 @@
+#Listas
+miLista = [2,3,5,7,8,5,4,5,4,6,67,65,6,5,5,5,5,5]
+
+print(miLista)
+
+if miLista[1] > miLista[3]:
+    print("El primero que dices es mayor que el segundo que me dices")
+    miLista[1] = 0
+    miLista[3] = 0
+
+print(miLista)
+
+#tuplas
+
+miTupla = [3,4,5,67,78,6,5,4,34,9]
+print(miTupla)
+
+if miTupla[1] > miTupla[3]:
+    print("El primero que dices es mayor que el segundo que me dices")
+
+#conjuntos
+
+miConjunto = {3,4,5,6,5,6,7,3,87,3,3,3,3,3,3,3,3,3,3,3}
+print("conjunto")
+print(miConjunto)\
+
+#diccionarios
+
+miDiccionario = {"nombre" : "Dani","edad":21}
